d4f97d12bb63e1fb3ae9c4144d87a3b5d11acf5f
anacamilly <camillyguedes2@gmail.com>
2022-04-17 20:29:33 -0300

    Adding product search

--- javascript/app.js
+++ javascript/app.js
@@ -49,5 +49,37 @@ const checkout = phoneNumber => {
 }
 
 
+const search = (products, searchTerm) => products.filter( product => product.title.includes(`${searchTerm}`) ||
+product.description.includes(`${searchTerm}`))
+
+const loadSearch = (form, productsDivId) => {
+    const productsDiv = document.querySelector(productsDivId)
+    const inputSearch = form.querySelector('#inputSearch')
+   
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        if(inputSearch.value != ''){
+            productsDiv.querySelectorAll('.prato').forEach(prato => {
+                prato.remove()
+            })
+            const results = search(produtos, inputSearch.value)
+
+            results.forEach( produto => {
+                const html = `
+                <article class="prato">
+                <img src="${produto.image}" alt="${produto.title}">
+                <h4>${produto.title}</h4>
+                <h4>R$ ${produto.value}</h4>
+                <p>${produto.description}</p>
+                <button type="button" onclick="modalTrigger(${produto.id})">Quero este prato</button>
+                </article>
+                `
+                productsDiv.insertAdjacentHTML('Beforeend', html)
+            })
+        }
+    })
+}
+
 loadProducts(produtos, '#product-div')
 checkout('5584900000000')
+loadSearch(document.querySelector('#formSearch'), '#product-div')
